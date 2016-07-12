0700accbb9648a84b68c7c5e1045d3cec7cf1801
Brent Perry <burentoperii@gmail.com>
2016-07-12 10:41:01 -0500

    Fix calculations for high/low temps.

--- TempData/main.cpp
+++ TempData/main.cpp
@@ -6,10 +6,27 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    
+    vector<double> temps;
+    
+    for (double temp; cin >> temp;)
+        temps.push_back(temp);
+    
+    double sum = 0;
+    double high_temp = double(temps[0]);
+    double low_temp = double(temps[0]);
+    
+    for (int x : temps) {
+        if (x > high_temp) high_temp = x;
+        if (x < low_temp) low_temp = x;
+        sum += x;
+    }
+    
+    cout << "High temperature: " << high_temp << "\n";
+    cout << "Low temperature: " << low_temp << "\n";
+    cout << "Average temperature: " << sum / temps.size() << "\n";
+    
 }
